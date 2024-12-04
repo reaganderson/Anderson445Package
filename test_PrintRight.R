ddb1b016c3abfbfd2e6c5a2e6fcd4c35e81845a2
reagan a <reaganaanderson@gmail.com>
2024-12-04 09:43:02 -0700

    Delete test_PrintRight.R

--- test_PrintRight.R
+++
@@ -1,7 +0,0 @@
-# checks that FizzBuzz prints Fizz, Buzz and Fizz-Buzz correctly
-test_that('FizzBuzz prints Fizz, Buzz and Fizz-Buzz correctly', {
-  expect_equal( FizzBuzz(3), c(1, 2, "Fizz"))
-  expect_equal( FizzBuzz(10), c(1,2,"Fizz",4,"Buzz","Fizz",7,8,"Fizz","Buzz"))
-  expect_equal( FizzBuzz(20), c(1,2,"Fizz",4,"Buzz","Fizz",7,8,"Fizz","Buzz",
-                                11,"Fizz",13,14,"Fizz-Buzz",16,17,"Fizz",19,"Buzz"))
-})
